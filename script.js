1ac9ba68d0b8802a38eb8b69d139e884ea6c36e2
Jintapcode <phuongjin127@gmail.com>
2021-12-04 00:16:50 +0900

    add more

---
+++ script.js
@@ -0,0 +1,10 @@
+// thiết lập thanh tiêu đề khi cuộn xuống
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    })
+});
